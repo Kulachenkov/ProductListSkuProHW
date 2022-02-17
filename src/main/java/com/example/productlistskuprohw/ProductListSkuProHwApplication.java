75ed5fdf1128ed48bf4ec6fbae0311922692bdae
Kulachenkov <nik.k2710@gmail.com>
2022-02-17 12:59:49 +0300

    Initial commit

---
+++ src/main/java/com/example/productlistskuprohw/ProductListSkuProHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.productlistskuprohw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductListSkuProHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductListSkuProHwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/productlistskuprohw/ProductListSkuProHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productlistskuprohw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductListSkuProHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
